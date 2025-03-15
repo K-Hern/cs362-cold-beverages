7b14ea5e750db47acf2cd289efe47ed248b35d28
Kevin Hernandez <KFHEDU@gmail.com>
2025-03-14 19:19:52 -0700

    Adding Item specs

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,14 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
+  it "allows the creation of an instance" do
+    expect { Item.new("Test Item", 80) }.not_to raise_error
+  end
 
+  it "has a name and a volume" do
+    item = Item.new("Test Item", 80)
+
+    expect(item.name).to eq("Test Item")
+    expect(item.volume).to eq(80)
+  end
 end
