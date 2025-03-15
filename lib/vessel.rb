86a559fa24441eca993642176801b94be1d3b69c
Kevin Hernandez <KFHEDU@gmail.com>
2025-03-14 19:02:23 -0700

    Finished Vessel class based off of vessel_spec

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,17 @@
 class Vessel
-  attr_reader :volume
+  attr_reader :volume, :name
 
-  def initialize(name, volume)
+  def initialize(name = 'FAKE', volume = 100)
     @name = name
     @volume = volume
+    @contentsVolume = 0
   end
 
+  def empty?()
+    return (@contentsVolume == 0)
+  end
+
+  def fill()
+    @contentsVolume = @volume
+  end
 end
